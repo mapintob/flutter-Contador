f902b49bfe1cbe15e5b40d44ed25031cbc95ed93
FlutLab.io <>
2022-06-03 00:18:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // Application theme data, you can set the colors for the application as
         // you want
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo[200],
       ),
       home: MyHomePage(title: 'Flutter Demo Clicker Counter Home Page'),
     );
